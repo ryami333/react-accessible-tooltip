a02218b456b2483826cfa919bd9f578e654494f0
Mitch Ryan <mitch.ryan@springload.co.nz>
2017-09-15 22:51:09 +1200

    Add Container component

---
+++ demo/components/Container/Container.js
@@ -0,0 +1,11 @@
+// @flow
+
+import React, { type ElementProps } from 'react';
+import classnames from 'classnames';
+import './container.scss';
+
+function Container({ className, ...rest }: ElementProps<'div'>) {
+    return <div className={classnames('container', className)} {...rest} />;
+}
+
+export default Container;
--- demo/components/Header/Header.js
+++ demo/components/Header/Header.js
@@ -1,13 +1,16 @@
 // @flow
 
 import React from 'react';
+import Container from '../Container/Container';
 import './header.scss';
 
 function Header() {
     return (
         <header className="header">
-            <h1 className="header__title">React Accessible Tooltip</h1>
-            <h2 className="header__subtitle">Interactive Demo</h2>
+            <Container>
+                <h1 className="header__title">React Accessible Tooltip</h1>
+                <h2 className="header__subtitle">Interactive Demo</h2>
+            </Container>
         </header>
     );
 }
