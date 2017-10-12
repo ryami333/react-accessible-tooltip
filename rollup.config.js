26ba8d1bdb9ae8b978bac8e5fe0d611950349038
Mitch Ryan <mitch.ryan@outlook.co.nz>
2017-10-12 20:53:24 +1300

    Remove non-es builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,31 @@
 import resolve from 'rollup-plugin-node-resolve';
 import eslint from 'rollup-plugin-eslint';
 import babel from 'rollup-plugin-babel';
-import pkg from './package.json';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
 import flow from 'rollup-plugin-flow';
+import pkg from './package.json';
 
 export default [
-	{
-		entry: 'src/index.js',
-		external: ['react'],
-		targets: [
-			{ dest: pkg.browser, format: 'umd' },
-			{ dest: pkg.main, format: 'cjs' },
-			{ dest: pkg.module, format: 'es' },
-		],
-		moduleName: 'reactAccessibleTooltip',
-		plugins: [
-			resolve({
-				jsnext: true,
-				main: true,
-				browser: true,
-			}),
-			commonjs(),
-			eslint(),
-			flow(),
-			babel(),
-		    replace({
-				exclude: 'node_modules/**',
-				ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-		    }),
-		]
-	},
+    {
+        entry: 'src/index.js',
+        external: ['react'],
+        targets: [{ dest: pkg.main, format: 'es' }],
+        moduleName: 'reactAccessibleTooltip',
+        plugins: [
+            resolve({
+                jsnext: true,
+                main: true,
+                browser: true,
+            }),
+            commonjs(),
+            eslint(),
+            flow(),
+            babel(),
+            replace({
+                exclude: 'node_modules/**',
+                ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+            }),
+        ],
+    },
 ];
