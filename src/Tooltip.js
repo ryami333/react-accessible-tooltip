456bc45feb181e987eeebba58deaaf1ee3d305a5
Mitch Ryan <mitch.ryan@outlook.co.nz>
2017-10-12 14:23:14 +1300

    Shift types out of their own file

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -1,7 +1,19 @@
 // @flow
 
 import React, { Component } from 'react';
-import type { TooltipProps, TooltipState } from './Tooltip.flow';
+import type { Node, ElementProps } from 'react';
+
+export type TooltipState = {
+    isHidden: boolean,
+};
+
+export type LabelProps = ElementProps<'*'> & TooltipState;
+export type OverlayProps = ElementProps<'*'> & TooltipState;
+
+export type TooltipProps = ElementProps<'div'> & {
+    label: LabelProps => Node,
+    overlay: OverlayProps => Node,
+};
 
 let counter = 0;
 
