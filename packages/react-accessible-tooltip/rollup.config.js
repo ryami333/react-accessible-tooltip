35402796970589749059abe3b34418fee77e0c72
Mitch Ryan <ryami333@users.noreply.github.com>
2024-06-14 10:22:25 +0200

    Replace deprecated rollup dependencies

--- packages/react-accessible-tooltip/rollup.config.js
+++ packages/react-accessible-tooltip/rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import eslint from 'rollup-plugin-eslint';
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import replace from 'rollup-plugin-replace';
 import pkg from './package.json';
 
