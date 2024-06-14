9ae491d4a56e94e1eb60b5939355ba9393fbbd74
Mitch Ryan <ryami333@users.noreply.github.com>
2024-06-14 12:18:17 +0200

    Fix build script by updating Rollup config, remove redundant commonjs plugin.

--- packages/react-accessible-tooltip/rollup.config.mjs
+++ packages/react-accessible-tooltip/rollup.config.mjs
@@ -1,26 +1,29 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
 import replace from 'rollup-plugin-replace';
 import pkg from './package.json' with { type: 'json' };
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
 export default [
     {
         input: 'src/index.ts',
         external: ['react'],
         output: [
-            { file: pkg.main, format: 'umd' },
+            { file: pkg.main, format: 'cjs' },
             { file: pkg['jsnext:main'], format: 'es' },
         ],
         name: 'reactAccessibleTooltip',
         plugins: [
-            babel(),
+            babel({
+                extensions,
+            }),
             resolve({
                 jsnext: true,
                 main: true,
                 browser: true,
+                extensions,
             }),
-            commonjs(),
             replace({
                 exclude: 'node_modules/**',
                 ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
