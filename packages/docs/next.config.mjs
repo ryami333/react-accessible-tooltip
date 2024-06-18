663e60a065e95053cf83444e42b1e7f1b97ae4a1
Mitch Ryan <ryami333@users.noreply.github.com>
2024-06-18 21:37:42 +0200

    Add basePath and assetPrefix config for Github Pages

--- packages/docs/next.config.mjs
+++ packages/docs/next.config.mjs
@@ -1,8 +1,13 @@
+/* eslint-env node */
+const isProd = process.env.NODE_ENV === 'production';
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
     output: 'export',
+    basePath: isProd ? '/react-accessible-tooltip' : undefined,
+    assetPrefix: isProd ? '/react-accessible-tooltip/' : undefined,
 };
 
 export default nextConfig;
